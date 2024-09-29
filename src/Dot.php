fe30c7cf9013c6d0bc90f3037b837e51cd723f6c
Martin <hello@northrook.com>
2024-09-29 15:07:57 +0100

    mend

--- src/Dot.php
+++ src/Dot.php
@@ -5,7 +5,7 @@ namespace Northrook;
 use Northrook\ArrayAccessor\ArrayConstructor;
 use Northrook\ArrayAccessor\PublicSetArrayValue;
 
-class Dot extends ArrayAccessor
+class Dot extends ArrayAgccessor
 {
     use ArrayConstructor, PublicSetArrayValue;
 }
